fbe5addd876e88138cdb84ed4cdc72e3b06ba0af
tiphergane <tiphergane@gmail.com>
2021-01-09 20:33:02 +0100

    modifié :         gestion/ajout.php 	modifié :         gestion/prendre.php

--- gestion/ajout.php
+++ gestion/ajout.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<?php include '../header.php'; ?>
+<?php require '../header.php'; ?>
 
 <div id="description">
-	<p>Dans Amazon, merci de mettre le lien vers le descriptif de l'article. </p>
-	<p>Si c'est sur Amazon, mettre https://www.amazon.fr/dp/XXXXXXXX ou XXXXXXX est la série après le /dp/</p>
-	<p> /!\ TOUTES LES LIGNES DOIVENT ÊTRE REMPLIE /!\ </p>
+    <p>Dans Amazon, merci de mettre le lien vers le descriptif de l'article. </p>
+    <p>Si c'est sur Amazon, mettre https://www.amazon.fr/dp/XXXXXXXX ou XXXXXXX est la série après le /dp/</p>
+    <p> /!\ TOUTES LES LIGNES DOIVENT ÊTRE REMPLIE /!\ </p>
 </div>
 <div id="formulaire">
                 <form method="POST" action="ajout.php">
@@ -16,33 +16,31 @@
                 <input type="submit" name="bt" value="valider"><br>
                 </form>
         </div>
-        <?php include("../cnx.php");?>
+        <?php require "../cnx.php";?>
         <?php
 
                 $conn = new mysqli($servername, $username, $password, $dbname);
 
-		                                if ($conn->connect_error) {
-							                                die("Connection failed: " . $conn->connect_error);
-											                                }
+        if ($conn->connect_error) {
+             die("Connection failed: " . $conn->connect_error);
+        }
 
-/* Si les champs ne sont pas vide, alors on se connecte à la BDD et on envoie les infos */
-if(!empty($_POST["nom"] && $_POST["cadeau"] && $_POST["amazon"]))
-{
-$nom=htmlspecialchars(mysqli_real_escape_string($conn,$_POST['nom']));
-$cadeau=htmlspecialchars(mysqli_real_escape_string($conn,$_POST['cadeau']));
-$amazon=htmlspecialchars(mysqli_real_escape_string($conn,$_POST['amazon']));
-$sql = "INSERT INTO wishes (NOM,CADEAU,AMAZON) VALUES ('$nom','$cadeau','$amazon')";
-if($conn->query($sql)===TRUE)
-{
-echo"<div id='resultat'>";
-echo"<font face='Verdana' size='3' >L'élément a bien été inséré !</font>";
-echo"</div>";
-}
-else    {
-echo "<div id='resultat'>";
-echo "<font face='Verdana' size='3' > Error: " .$sql. "<br />" . $conn->error;
-}
-}
-mysqli_close($conn);
-?>
-<?php include '../footer.php'; ?>
+        /* Si les champs ne sont pas vide, alors on se connecte à la BDD et on envoie les infos */
+        if(!empty($_POST["nom"] && $_POST["cadeau"] && $_POST["amazon"])) {
+            $nom=htmlspecialchars(mysqli_real_escape_string($conn, $_POST['nom']));
+            $cadeau=htmlspecialchars(mysqli_real_escape_string($conn, $_POST['cadeau']));
+            $amazon=htmlspecialchars(mysqli_real_escape_string($conn, $_POST['amazon']));
+            $sql = "INSERT INTO wishes (NOM,CADEAU,AMAZON) VALUES ('$nom','$cadeau','$amazon')";
+            if($conn->query($sql)===true) {
+                echo"<div id='resultat'>";
+                echo"<font face='Verdana' size='3' >L'élément a bien été inséré !</font>";
+                echo"</div>";
+            }
+            else    {
+                echo "<div id='resultat'>";
+                echo "<font face='Verdana' size='3' > Error: " .$sql. "<br />" . $conn->error;
+            }
+        }
+        mysqli_close($conn);
+        ?>
+<?php require '../footer.php'; ?>
--- gestion/prendre.php
+++ gestion/prendre.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<?php include '../header.php'; ?>
+<?php require '../header.php'; ?>
 
 <div id="description">
 <p>Ici vous choisissez le(s) cadeau(x) que vous voulez prendre en charge</p>
@@ -18,55 +18,53 @@
         </div>
 
 <br />
-<?php include("../cnx.php"); ?>
+<?php require "../cnx.php"; ?>
 
 <?php
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-if(!empty($_POST["nom"] && $_POST["ID"]))
-{
-$nd=htmlspecialchars(mysqli_real_escape_string($conn,$_POST['nom']));
-$puk=htmlspecialchars(mysqli_real_escape_string($conn,$_POST['ID']));
-$sql = "UPDATE wishes SET PRIS='$nd'WHERE ID='$puk'";
-if($conn->query($sql)===TRUE)
-{
-echo"<div id='resultat'>";
-echo"<font face='Verdana' size='3' >L'élément a bien été inséré !</font>";
-echo"</div>";
-}
-else    {
-echo "<div id='resultat'>";
-echo "<font face='Verdana' size='3' > Error: " .$sql. "<br />" . $conn->error;
-}
+if(!empty($_POST["nom"] && $_POST["ID"])) {
+    $nd=htmlspecialchars(mysqli_real_escape_string($conn, $_POST['nom']));
+    $puk=htmlspecialchars(mysqli_real_escape_string($conn, $_POST['ID']));
+    $sql = "UPDATE wishes SET PRIS='$nd'WHERE ID='$puk'";
+    if($conn->query($sql)===true) {
+        echo"<div id='resultat'>";
+        echo"<font face='Verdana' size='3' >L'élément a bien été inséré !</font>";
+        echo"</div>";
+    }
+    else    {
+        echo "<div id='resultat'>";
+        echo "<font face='Verdana' size='3' > Error: " .$sql. "<br />" . $conn->error;
+    }
 }
 mysqli_close($conn);
 ?>
 
 <div id="tabget">
-<?php include("cnx.php");
+<?php require "cnx.php";
                  // on se connecte à MySQL
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 // on crée la requête SQL
 $sql = "SELECT * FROM wishes WHERE PRIS=''";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
-echo "<TABLE><tr><th>ID</th><th>Qui</th><th>Quoi</th><th>OK</th></tr>";
-// output data of each row
-while($row = $result->fetch_assoc()) {
-echo "<tr><td>" . $row["ID"]. "</td><td>" . $row["NOM"]. "</td><td>" . $row["CADEAU"]. "</td><td> " . $row["PRIS"]."</td></tr>";
-}
-echo "</TABLE><br />";
+    echo "<TABLE><tr><th>ID</th><th>Qui</th><th>Quoi</th><th>OK</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row["ID"]. "</td><td>" . $row["NOM"]. "</td><td>" . $row["CADEAU"]. "</td><td> " . $row["PRIS"]."</td></tr>";
+    }
+    echo "</TABLE><br />";
 } else {
-echo "0 results<br />";
+    echo "0 results<br />";
 }
 // on ferme la connexion à mysql
 mysqli_close($conn);
 ?>
 </div>
-<?php include '../footer.php'; ?>
+<?php require '../footer.php'; ?>
 
 
