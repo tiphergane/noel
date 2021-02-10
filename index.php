0a1eda1f6cfb0cde2eefa5b5ffde87d0e503c95c
tiphergane <tiphergane@gmail.com>
2021-02-10 20:10:42 +0100

    Modification mineure d'index.php

--- index.php
+++ index.php
@@ -9,31 +9,31 @@
 <p>
 <div id="tableau">
                 <h1> Liste de Noel de la Kitten Familly </h1>
-                <?php require "cnx.php";
-                 // on se connecte à MySQL
-                $conn = new mysqli($servername, $username, $password, $dbname);
+<?php require "cnx.php";
+// on se connecte à MySQL
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-                if ($conn->connect_error) {
-                    die("Connection failed: " . $conn->connect_error);
-                }
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
                 // on crée la requête SQL
                 $sql = "SELECT * FROM wishes";
                 $result = $conn->query($sql);
 
-                if ($result->num_rows > 0) {
-                    echo "<TABLE><tr><th>Nom</th><th>Quoi</th><th>Lien</th><th>OK</th></tr>";
-                    // output data of each row
-                    while($row = $result->fetch_assoc()) {
-                        echo "<tr><td>" . $row["NOM"]. "</td><td>" . $row["CADEAU"]. "</td><td> " . $row["AMAZON"]. "</td><td>". $row["PRIS"]."</td></tr>";
-                    }
-                    echo "</TABLE><br />";
-                } else {
-                    echo "0 results<br />";
-                }
+if ($result->num_rows > 0) {
+    echo "<TABLE><tr><th>Nom</th><th>Quoi</th><th>Lien</th><th>OK</th></tr>";
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row["NOM"]. "</td><td>" . $row["CADEAU"]. "</td><td> " . $row["AMAZON"]. "</td><td>". $row["PRIS"]."</td></tr>";
+    }
+    echo "</TABLE><br />";
+} else {
+    echo "0 results<br />";
+}
                 // on ferme la connexion à mysql
                 mysqli_close($conn);?>
-</div>
-</p>
-<br />
-<?php require 'footer.php'; ?>
+                </div>
+                    </p>
+                    <br />
+                    <?php require 'footer.php'; ?>
